683e8c7dfc194b2c276d9650a31c2f3322641941
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-09-05 21:14:11 +0200

    change redis_queue_host to redis_host

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8080"]
 
 CMD ["python", "worker.py"]
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from rq import Queue
 from redis import Redis
@@ -6,18 +8,20 @@ from rq.job import Job
 app = FastAPI()
 
 # Establish connection to Redis
-redis_host = "redis"  # This will be your container name for Redis in Docker
+redis_host = os.getenv('REDIS_HOST', 'localhost')
 redis_conn = Redis(host=redis_host, port=6379)
 queue = Queue(connection=redis_conn)
 
+
 @app.post("/tasks/")
 def enqueue_task(duration: int):
-    """Endpoint to receive duration and enqueue a task."""
-    job = queue.enqueue('tasks.background_task', duration)
-    return {"job_id": job.id, "status": "Task enqueued"}
+	"""Endpoint to receive duration and enqueue a task."""
+	job = queue.enqueue('tasks.background_task', duration)
+	return {"job_id": job.id, "status": "Task enqueued"}
+
 
 @app.get("/tasks/{job_id}")
 def get_task_status(job_id: str):
-    """Endpoint to check the status of a task."""
-    job = Job.fetch(job_id, connection=redis_conn)
-    return {"job_id": job_id, "status": job.get_status(), "result": job.result}
+	"""Endpoint to check the status of a task."""
+	job = Job.fetch(job_id, connection=redis_conn)
+	return {"job_id": job_id, "status": job.get_status(), "result": job.result}
